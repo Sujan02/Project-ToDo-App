33285f07bfba5de53a3915a83b8e00365fe5e281
Sujan Matroja <sujan.matroja@gmail.com>
2018-11-30 10:42:32 -0500

    Day-3 Form edit and dummy data

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,18 @@ const express = require('express');
 
 //requiring bodyparser
 const bodyParser = require('body-parser');
+//requiring mongoose
+const mongoose = require('mongoose');
 //module.exports =function(app){
 //creating express router to handle routes
 const router = express.Router();
 //const router = app.Router();
-const data = [{item: 'get Milk'}, {item: 'walk dog'}, {item: 'complete certification'}];
+
+
+//connect to the database
+
+//dummy data on the server, & will be passed to the todo view
+var data = [{item: 'get Milk'}, {item: 'walk dog'}, {item: 'complete certification'}];
 
 //create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -25,7 +32,7 @@ router.get('/todolists',function(req, res){
   //res.send({type: 'GET'});//this will send the response back to the browser with name property
   //res.render('todo');
   //after adding the data outputing interval
-  res.render('todo', {todos: data});
+  res.render('todo', {todos: data});//'todo' is the view and the array in data is passed to that
 });
 
 //Route by ID -- replacing router with app
@@ -34,12 +41,13 @@ router.get('/todolists/:id',function(req, res){
 //  res.end();//this will end the request
   res.send({type: 'GET'});//this will send the response back to the browser with name property
 });
-
+/*
 router.get('/todolists',function(req, res){
   //console.log('listening to get request');
 //  res.end();//this will end the request
   res.send({type: 'GET'});//this will send the response back to the browser with name property
 });
+*/
 //add a new todo list task
 router.post('/todolists', urlencodedParser ,function(req, res){
   //res.send({type: 'POST'});//this will send the response back to the browser with name property
@@ -53,8 +61,12 @@ router.put('/todolists/:id',function(req, res){
 });
 
 //deleting a todo list task
-router.delete('/todolists/:id',function(req, res){
-  res.send({type: 'DELETE'});//this will send the response back to the browser with name property
+router.delete('/todolists/:item',function(req, res){
+  //res.send({type: 'DELETE'});//this will send the response back to the browser with name property
+ data = data.filter(function(todo){
+   return todo.item.replace(/ /g, '-') !==req.params.item;
+ })
+ res.json(data);
 });
 
 
